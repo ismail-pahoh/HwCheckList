db56a3cfb3f9399dd37c61d53539f0bafb780d50
ismail-pahoh <bangismail@gmail.com>
2025-08-17 19:38:46 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('gas-pwa').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
 
